00dcc3c713d1db521814297dbb646560dc2f7ce7
Nadir Derdour <nadir.derdour@gmail.com>
2018-04-03 17:37:18 +0300

    added google weather api

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,11 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from "../actions/index.js";
 
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // return state.concat([ action.payload.data ]);
+      // NEVER manipulate state, create an entirely new array
+      return [ action.payload.data, ...state ]; // [ city, city, city ] NOT [ city, [ city, city ] ]
+  }
   return state;
 }
